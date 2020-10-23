93fe9c7b5a27e35140743469d526282eb0cc2e88
Gina Sprint <sprint@gonzaga.edu>
2020-10-23 11:40:16 -0700

    finish implementing data source methods

--- TableViewFunS2/DogTableViewController.swift
+++ TableViewFunS2/DogTableViewController.swift
@@ -17,8 +17,7 @@ import UIKit
 // we will use #1 because ADS uses #2
 
 
-class DogTableViewController: UIViewController {
-    
+class DogTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var dogs = [Dog]()
     
     @IBOutlet var tableView: UITableView!
@@ -34,6 +33,38 @@ class DogTableViewController: UIViewController {
         dogs.append(Dog(name: "AirBud", breed: "Retriever"))
         dogs.append(Dog(name: "Spike", breed: "Bulldog"))
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // table view is asking its data source
+        // "how many rows should I put in this section?"
+        // we are only going to have one section
+        if section == 0 {
+            return dogs.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // table view is asking its data source
+        // "what cell should I put at this indexPath?"
+        // IndexPath has 2 properties
+        // section number (we will ignore...)
+        // row number (corresponds to array index number)
+        let row = indexPath.row
+        let dog = dogs[row]
+        
+        // we want a cell that display's this dog's name and breed
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DogCell", for: indexPath) as! DogTableViewCell
+        
+        // why a reusable cell?
+        // what if dogs had 10000 dogs in it?
+        // we would not need 10000 cells because 10000 cells would not be in the table view at one time
+        
+        cell.update(with: dog)
+        
+        return cell
+    }
+    
 
 }
 
