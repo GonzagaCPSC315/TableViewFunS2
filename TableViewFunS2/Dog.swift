18abe005ee11ac883915df38bbb9524e5e38a079
Gina Sprint <sprint@gonzaga.edu>
2020-11-02 11:51:25 -0800

    finish archiving example

--- TableViewFunS2/Dog.swift
+++ TableViewFunS2/Dog.swift
@@ -11,10 +11,61 @@ import Foundation
 // author credit for dog image
 // <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 
-class Dog {
+// MARK: - Persistent Data Storage
+// users expect their data to persist across runs of the app
+// 2 main classifications of data storage
+// 1. local
+// 2. remote
+
+// iOS has a few options for local storage
+// 1. UserDefaults: used for storing simple types in a dictionary (e.g. a list of key value pairs)
+// great for storing a game high score or perhaps the volume setting for playing audio in the app
+// 2. Archiving: ADS 4.7 reading and writing Codable objects from and to the file system
+// it is inefficient for large files because if you only need to access one object in the file, you still need to load/save the entire file
+// 3. SQLite database: is a light weight relational database that uses SQL (structured query language)
+// 4. Core Data: an Apple framework that is an OOP wrapper for a data store. by default on iOS, data store is a SQLite database
+
+// remote options: backend as a service (BaaS) like Firebase or Realm, or host your own server...
+
+// MARK: - Archiving
+// most standard types (like String) conform to the Codable protocol
+// game plan
+// 1. get a URL for a dogs.plist file that stores our dog array
+// think of a URL like a file path except the application sandbox causes the URL to change (for security purposes)
+// 2. write a static method to save the dogs array to dogs.plist
+// 3. write a static method to load the dogs into an array from dogs.plist
+
+class Dog: Codable {
     var name: String
     var breed: String
     var imageName: String
+    
+    static let pListURL: URL = {
+       // this is called an initialization closure
+        let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        // userDomainMask refers to the user's home directory
+        let fileURL = documentsDirectoryURL.appendingPathComponent("dogs").appendingPathExtension("plist")
+        return fileURL
+    }()
+    
+    static func saveDogsToFile(dogs: [Dog]) {
+        let encoder = PropertyListEncoder()
+        
+        if let dogsData = try? encoder.encode(dogs) {
+            // dogsData is of type Data (byte representation)
+            try? dogsData.write(to: pListURL)
+        }
+        
+    }
+    
+    static func loadDogsFromFile() -> [Dog]? {
+        let decoder = PropertyListDecoder()
+        
+        if let dogsData = try? Data(contentsOf: pListURL), let decodedDogs = try? decoder.decode([Dog].self, from: dogsData) {
+            return decodedDogs
+        }
+        return nil 
+    }
     
     init(name: String, breed: String) {
         self.name = name
--- TableViewFunS2/DogTableViewController.swift
+++ TableViewFunS2/DogTableViewController.swift
@@ -18,20 +18,32 @@ import UIKit
 
 
 class DogTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    var dogs = [Dog]()
+    var dogs = [Dog]() {
+        didSet {
+            Dog.saveDogsToFile(dogs: dogs)
+            // another place we could call this method
+            // applicationDidEnterBackground()
+        }
+    }
     
     @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(Dog.pListURL)
         initializeDogs()
     }
 
     func initializeDogs() {
-        dogs.append(Dog(name: "Lassie", breed: "Collie"))
-        dogs.append(Dog(name: "AirBud", breed: "Retriever"))
-        dogs.append(Dog(name: "Spike", breed: "Bulldog"))
+        if let decodedDogs = Dog.loadDogsFromFile() {
+            dogs = decodedDogs
+        }
+        else {
+            dogs.append(Dog(name: "Lassie", breed: "Collie"))
+            dogs.append(Dog(name: "AirBud", breed: "Retriever"))
+            dogs.append(Dog(name: "Spike", breed: "Bulldog"))
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
